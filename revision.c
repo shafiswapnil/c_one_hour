8417be7156fffb40d44a95559176c13e40661c53
Shafi Swapnil <swapnil.env@gmail.com>
2022-09-10 21:47:44 +0600

    added revision file to practice previous topics

---
+++ revision.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* REVISE TOPICS
+ * variables naming
+ * data types
+ * bit/bytes 
+ * typecasting
+ * arithmatic
+ * odd/even
+ * getchar()
+ */
+
+int main () {
+  printf("Hello world\n");
+
+  return 0;
+}
