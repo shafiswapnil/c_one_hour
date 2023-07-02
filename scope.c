b8846ce96d7014a5ac1f48334acd4a245906aa50
Shafi Swapnil <swapnil.env@gmail.com>
2023-07-02 23:16:12 +0600

    scope learned

--- scope.c
+++ scope.c
@@ -3,12 +3,19 @@
 // VARIABLE DECLATION
 // int a = 6;
 int b = 7;
+int n = 31;
 
 // sample function
-void saySomething()
+void functionOne()
 {
   int m = 90;
   printf("m = %d\n", m);
+}
+
+void functionTwo()
+{
+  printf("n (from functionTwo) = %d\n", n);
+  // see here we can access the n variable from anywhere (1)
 }
 
 int main()
@@ -21,10 +28,13 @@ int main()
   printf("b = %d\n", b);
   // printing the global variable b (which declared outside of any block)
 
-  saySomething();
-
+  functionOne();
   // printf("m = %d\n", m);     // error: use of undeclared identifier 'm'
   // this printf cannot print the value of m because m is declared abobe in another block (which is local to that function block only)
 
-    return 0;
+  functionTwo();
+  printf("n (inside main function) = %d\n", n);
+  // see here we can access the n variable from anywhere (2)
+
+  return 0;
 }
