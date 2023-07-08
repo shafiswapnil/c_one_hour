6a430511b8ce37abe1333d33b94162406eedf5fd
Shafi Swapnil <swapnil.env@gmail.com>
2023-07-08 13:21:12 +0600

    struct typedef init

---
+++ struct.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef
+
+    struct Person
+{
+  char name[50];
+  int age;
+  int weight;
+  int height;
+}
+
+int
+main()
+{
+
+  return 0;
+}
+
+/* STRUCT
+ * it's a custom datatype. (not a class)
+ * when to call the struct, we need to use `struct structIdentifier demoName;`
+ */
+
+/* TYPEDEF
+ * it's needed because of struct. when we call struct we have to use struct all the time.
+ *
+ */
